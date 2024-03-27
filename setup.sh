f58d0800a007ee0268fd7a27b4071ea78e904e42
xiaoua <xiaoua@node-0.vm-migration.xdp-pg0.utah.cloudlab.us>
2024-03-27 13:32:07 -0600

    add qemu install

--- setup.sh
+++ setup.sh
@@ -1,17 +1,38 @@
 sudo apt update
 
 # qemu dependency.
-sudo apt install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev python3-venv ninja-build flex bison
+sudo apt install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev python3-venv ninja-build flex bison -y
 
 # recommended.
-sudo apt-get install git-email
-sudo apt-get install libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev
-sudo apt-get install libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev
-sudo apt-get install libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev
-sudo apt-get install librbd-dev librdmacm-dev
-sudo apt-get install libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev
-sudo apt-get install libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev
-sudo apt-get install valgrind xfslibs-dev 
-sudo apt-get install libnfs-dev libiscsi-dev
+sudo apt-get install git-email -y
+sudo apt-get install libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev -y
+sudo apt-get install libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev -y
+sudo apt-get install libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev -y
+sudo apt-get install librbd-dev librdmacm-dev -y
+sudo apt-get install libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev -y
+sudo apt-get install libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev -y
+sudo apt-get install valgrind xfslibs-dev -y
+sudo apt-get install libnfs-dev libiscsi-dev -y
 
+
+# check kvm support
+if [ $(egrep -c '(vmx|svm)' /proc/cpuinfo) -gt 0 ]; then
+    echo "KVM is supported"
+else 
+    echo "KVM is supported"
+    exit 1
+fi
+
+if lsmod | grep kvm; then 
+    echo "KVM module loaded"
+else 
+    # sudo modprobe kvm-intel
+    # sudo modprobe kvm-amd
+    echo "KVM module not loaded"
+fi
+
+cd qemu-master
+./configure --target-list=x86_64-softmmu --enable-kvm
+make -j
+sudo make install
 
