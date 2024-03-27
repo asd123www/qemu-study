9edd7c8bcd038502c7947646c082e5419f1560e7
xiaoua <xiaoua@node-1.vm-migration.xdp-pg0.utah.cloudlab.us>
2024-03-27 12:20:10 -0600

    add qemu dependency

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt update
+
+# qemu dependency.
+sudo apt install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev python3-venv ninja-build flex bison
+
+# recommended.
+sudo apt-get install git-email
+sudo apt-get install libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev
+sudo apt-get install libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev
+sudo apt-get install libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev
+sudo apt-get install librbd-dev librdmacm-dev
+sudo apt-get install libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev
+sudo apt-get install libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev
+sudo apt-get install valgrind xfslibs-dev 
+sudo apt-get install libnfs-dev libiscsi-dev
+
+
