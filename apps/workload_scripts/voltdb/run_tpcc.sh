33d3a598dfdf5c751ff71ff656ceeaf59626437d
asd123www <352178414@qq.com>
2024-10-30 12:13:03 -0600

    add tpcc displayinterval overwrite

--- apps/workload_scripts/voltdb/run_tpcc.sh
+++ apps/workload_scripts/voltdb/run_tpcc.sh
@@ -1,16 +1,17 @@
 source config.txt
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
     echo "Error: No parameters provided."
-    echo "Usage: $0 <duration(seconds)> <# of warehouses>"
+    echo "Usage: $0 <duration(seconds)> <# of warehouses> <displayinterval(milliseconds)>"
     exit 1
 fi
 
 # the first
 duration=$1
 warehouses=$2
+displayinterval=$3
 
 cd apps/voltdb/voltdb/tests/test_apps/tpcc
-sudo bash run.sh client $duration $warehouses $SHARED_STORAGE/config.txt
+sudo bash run.sh client $duration $warehouses $displayinterval $SHARED_STORAGE/config.txt
 
 echo "Finished VoltDB TPC-C benchmark."
